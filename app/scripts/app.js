3cff95d3c188efbdd50894291d45e065ac890e08
RemyHannequin <remy.hannequin@gmail.com>
2013-02-26 23:32:23 +0100

    Add checkboxes list + Opa Backbone.js Style

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,75 +1,155 @@
-/*global define */
-define([], function () {
+define(['backbone'], function (Backbone) {
   'use strict';
 
-  if (navigator.geolocation) {
-    var apiUrl = 'https://api.foursquare.com/v2/',
-      oauth_token  = 'L3DMOWACH200L0QKG1GK5DVGS1EEFMO5K4ND5WCI3JQ1RZ3I';
+  var MainView = Backbone.View.extend({
 
-    navigator.geolocation.getCurrentPosition(function (position) {
+    el: 'body',
+    foursquareApiUrl: 'https://api.foursquare.com/v2/',
+    foursquareOauthToken: 'L3DMOWACH200L0QKG1GK5DVGS1EEFMO5K4ND5WCI3JQ1RZ3I',
+    foursquareFoodCategoryId: '4d4b7105d754a06374d81259',
+    foursquareCategories: $('#require-js').data('params').foursquareCategories,
+    /*foursquareCategories: [
+      {name: "Boulangerie",                      id: "4bf58dd8d48988d16a941735"},
+      {name: "Brasserie",                        id: "50327c8591d4c4b30a586d5d"},
+      {name: "Lieu servant des hamburgers",      id: "4bf58dd8d48988d16c941735"},
+      {name: "Restaurant chinois",               id: "4bf58dd8d48988d145941735"},
+      {name: "Café-restaurant",                  id: "4bf58dd8d48988d147941735"},
+      {name: "Fast-food",                        id: "4bf58dd8d48988d16e941735"},
+      {name: "Restaurant français",              id: "4bf58dd8d48988d10c941735"},
+      {name: "Restaurant grec",                  id: "4bf58dd8d48988d10e941735"},
+      {name: "Restaurant indien",                id: "4bf58dd8d48988d10f941735"},
+      {name: "Restaurant japonais",              id: "4bf58dd8d48988d111941735"},
+      {name: "Restaurant coréen",                id: "4bf58dd8d48988d113941735"},
+      {name: "Pizzeria",                         id: "4bf58dd8d48988d1ca941735"},
+      {name: "Sandwicherie",                     id: "4bf58dd8d48988d1c5941735"},
+      {name: "Restaurant de fruits de mer",      id: "4bf58dd8d48988d1ce941735"},
+      {name: "Snack",                            id: "4bf58dd8d48988d1c7941735"},
+      {name: "Restaurant-grill",                 id: "4bf58dd8d48988d1cc941735"},
+      {name: "Bar à sushis",                     id: "4bf58dd8d48988d1d2941735"},
+      {name: "Bar à tapas",                      id: "4bf58dd8d48988d1db931735"},
+      {name: "Restaurant thaïlandais",           id: "4bf58dd8d48988d149941735"},
+      {name: "Restaurant turc",                  id: "4f04af1f2fb6e1c99f3db0bb"},
+      {name: "Restaurant végétarien/végétalien", id: "4bf58dd8d48988d1d3941735"}
+    ],*/
+    map: null,
+
+    events: {
+      'click .find-venues': 'fireVenuesSearch'
+    },
+
+    initialize: function () {
+      var self = this;
+      if (this.geolocIsAvailable()) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          self.renderMap(position);
+          self.showCategoryList(self.foursquareCategories);
+        });
+      } else {
+        console.warn('You don\'t have HTML Geolocation API available on this browser.');
+      }
+    },
+
+    geolocIsAvailable: function () {
+      return typeof navigator.geolocation !== 'undefined';
+    },
+
+    renderMap: function (position) {
       // User's location
-      var latitude = position.coords.latitude,
-          longitude = position.coords.longitude;
-      var center = new google.maps.LatLng(latitude, longitude);
+      this.userLatitude = position.coords.latitude;
+      this.userLongitude = position.coords.longitude;
+
+      var center = new google.maps.LatLng(this.userLatitude, this.userLongitude);
       var mapOptions = {
         center: center,
-        zoom: 13,
+        zoom: 14,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
-      var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+      this.map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
+      this.renderUserLocation(center, this.map);
+    },
 
+    renderUserLocation: function (position, map) {
+      var customMarker = this.customMarker();
+      var marker = new google.maps.Marker({
+        position: position,
+        map: this.map,
+        title: 'You are here!',
+        icon: customMarker[0],
+        shadow: customMarker[1]
+      });
+    },
+
+    foursquareRequest: function (url, params) {
+      var params = params || {};
+      params.oauth_token = this.foursquareOauthToken;
+      return $.ajax({
+        url: this.foursquareApiUrl + url,
+        dataType: 'jsonp',
+        type: 'get',
+        data: params
+      });
+    },
+
+    fireVenuesSearch: function (e) {
+      var choices = []
+          self = this;
+      $( $(e.currentTarget).parent().find('input:checked') ).each(function (k, v) {
+        choices.push($(v).attr('name'));
+      });
+      choices = choices.join(',');
+
+      var venuesRequest = this.foursquareRequest('venues/search', {
+        ll: this.userLatitude + ',' + this.userLongitude,
+        intent: 'browse',
+        limit: 50,
+        radius: 5000, // 5000 meters around user's location
+        categoryId: choices
+      }).done(function (results) {
+        var items = results.response.groups[0].items;
+        self.showNearVenues(self.map, items);
+      });
+    },
+
+    showNearVenues: function (map, items) {
+      for(var k in items) {
+        var item = items[k];
+        var location = item.location;
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(location.lat, location.lng),
+          map: map,
+          title: item.name
+        });
+      }
+    },
+
+    showCategoryList: function (categories) {
+      var inputs = '';
+      for(var i in categories) {
+        var category = categories[i];
+        inputs += '<div class="row-fluid"><label><input type="checkbox" name="' + category.id + '" checked /> ' + category.name + '</label></div>';
+      }
+      $('.categories').append('<button class="btn find-venues">Find!</button>' + inputs);
+    },
+
+    customMarker: function () {
       // User's location marker
       // Credit: http://stackoverflow.com/users/3800/jack-b-nimble
       var pinColor = "37c855";
-      var pinImage = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
-        new google.maps.Size(21, 34),
-        new google.maps.Point(0, 0),
-        new google.maps.Point(10, 34)
+      var GMaps = google.maps;
+      var pinImage = new GMaps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|" + pinColor,
+        new GMaps.Size(21, 34),
+        new GMaps.Point(0, 0),
+        new GMaps.Point(10, 34)
       );
-      var pinShadow = new google.maps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
-        new google.maps.Size(40, 37),
-        new google.maps.Point(0, 0),
-        new google.maps.Point(12, 35)
+      var pinShadow = new GMaps.MarkerImage("http://chart.apis.google.com/chart?chst=d_map_pin_shadow",
+        new GMaps.Size(40, 37),
+        new GMaps.Point(0, 0),
+        new GMaps.Point(12, 35)
       );
+      return [pinImage, pinShadow];
+    }
 
-      var marker = new google.maps.Marker({
-        position: center,
-        map: map,
-        title: 'You are here!',
-        icon: pinImage,
-        shadow: pinShadow
-      });
+  });
 
-      var showNearVenues = function (items) {
-        for(var k in items) {
-          var item = items[k];
-          var location = item.location;
-          var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(location.lat, location.lng),
-            map: map,
-            title: item.name
-          });
-        }
-      };
-
-      var request = $.ajax({
-        url: apiUrl + 'venues/search',
-        dataType: 'jsonp',
-        type: 'get',
-        data: {
-          oauth_token: oauth_token,
-          ll: latitude + ',' + longitude,
-          intent: 'browse',
-          limit: 50,
-          radius: 5000, // 5000 meters around user's location
-          categoryId: '4bf58dd8d48988d1c4941735' // "Restaurant" categoryId
-        }
-      });
-
-      request.done(function (results) {
-        var items = results.response.groups[0].items;
-        showNearVenues(items);
-      });
-    });
-  }
+  return MainView;
 });
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,27 @@
 require.config({
   paths: {
     jquery: '../components/jquery/jquery',
-    bootstrap: 'vendor/bootstrap'
+    bootstrap: 'vendor/bootstrap',
+    backbone: '../components/backbone/backbone',
+    underscore: '../components/underscore/underscore'
   },
   shim: {
     bootstrap: {
       deps: ['jquery'],
       exports: '$'
+    },
+    underscore: {
+      exports: '_'
+    },
+    backbone: {
+      deps: ['underscore'],
+      exports: 'Backbone'
     }
   }
 });
 
-require(['app', 'jquery', 'bootstrap'], function (app, $) {
+require(['app', 'jquery', 'bootstrap'], function (MainView, $) {
   'use strict';
+
+  var mainView = new MainView();
 });
