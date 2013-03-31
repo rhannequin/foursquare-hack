6460edd17e21a2e206f6fd21015da5f350c9a8ed
rhannequin <remy.hannequin@gmail.com>
2013-03-31 17:28:35 +0200

    Add server launch and livereload

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,55 @@ module.exports = function (grunt) {
 
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
-  grunt.initConfig({});
+  // Grunt.js configuration
+  grunt.initConfig({
 
-  grunt.registerTask('default', []);
+    foursquareHack: foursquareHack,
+
+    // server
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: 'app'
+        }
+      }
+    },
+
+    // Livereload
+    livereload: {
+      port: 35728
+    },
+
+    // Watch
+    regarde: {
+      html: {
+        files: ['<%= foursquareHack.app.path %>/**/*.{html,md,txt,xml}'],
+        tasks: []
+      },
+      js: {
+        files: ['<%= foursquareHack.app.assets.scripts %>/**/*'],
+        tasks: []
+      },
+      css: {
+        files: ['<%= foursquareHack.app.assets.styles %>/**/*'],
+        tasks: []
+      },
+      images: {
+        files: ['<%= foursquareHack.app.assets.images %>/**/*'],
+        tasks: []
+      },
+      livereload: {
+        files: ['<%= foursquareHack.app.path %>/**/*'],
+        tasks: ['livereload']
+      }
+    }
+
+  });
+
+  // Default
+  grunt.registerTask('default', ['livereload-start', 'server', 'regarde']);
+
+  // Server
+  grunt.registerTask('server', 'connect:server');
 };
